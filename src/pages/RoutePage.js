5939d1c1cb6e31709a74def7b9deed499db6f810
On <cks3066@naver.com>
2021-12-03 02:44:56 +0900

    feat: 경로 검색 ui 구현

--- src/pages/RoutePage.js
+++ src/pages/RoutePage.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "../styles/Route.css";
+
+import searchBtn from "../img/searchButton.png";
 
 const RoutePage = (props) => {
-  return <>Route</>;
+  return (
+    <>
+      <div className="SearchContainer">
+        <div className="SearchInputContainer">
+          <input type="text" maxlength="13" />
+          <div className="hr" />
+          <input type="text" maxlength="13" />
+        </div>
+        <div id="searchBtn">
+          <img src={searchBtn} width="50px" class="searchBtn" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default RoutePage;
