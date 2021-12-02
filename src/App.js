919894e03043a1ebdc60d7fe8a5efba39de0a022
On <cks3066@naver.com>
2021-12-03 01:32:36 +0900

    feat: 기본 width, height 변경

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Route } from "react-router-dom";
 import "./styles/App.css";
 
 import Home from "./pages/Home";
+import RoutePage from "./pages/RoutePage";
 
 function App() {
   return (
     <div className="App">
       <Route path="/" exact component={Home} />
-      <Route path="/home" exact component={Home} />
+      <Route path="/route" exact component={RoutePage} />
     </div>
   );
 }
---
+++ src/pages/RoutePage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RoutePage = (props) => {
+  return <>Route</>;
+};
+
+export default RoutePage;
