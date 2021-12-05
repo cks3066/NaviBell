d8d64aa1e76e7836a4feb033051f82663a3bcd0f
On <cks3066@naver.com>
2021-12-06 01:29:24 +0900

    feat: axios 연결

--- src/pages/RoutePage.js
+++ src/pages/RoutePage.js
@@ -1,18 +1,55 @@
 import React from "react";
+import axios from "axios";
+
 import "../styles/Route.css";
 
 import searchBtn from "../img/searchButton.png";
 
+const handleSearchOnClick = (e) => {
+  axios
+    .get(
+      `https://api.odsay.com/v1/api/searchPubTransPath?SX=126.9027279&SY=37.5349277&EX=126.9145430&EY=37.5499421&apiKey=UPu+WjCg6qf1ZRG9oyRKzw`
+    )
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      console.dir(error);
+    });
+};
+
 const RoutePage = (props) => {
+  const [출발지, set출발지] = React.useState("");
+  const [도착지, set도착지] = React.useState("");
+
   return (
     <>
       <div className="SearchContainer">
         <div className="SearchInputContainer">
-          <input type="text" />
+          <input
+            type="text"
+            placeholder="출발지"
+            onChange={(e) => {
+              set출발지(e.target.value);
+            }}
+          />
           <div className="hr" />
-          <input type="text" />
+          <input
+            type="text"
+            placeholder="도착지"
+            onChange={(e) => {
+              set도착지(e.target.value);
+            }}
+          />
         </div>
-        <img src={searchBtn} width="30px" height="30px" class="searchBtn" />
+        <img
+          src={searchBtn}
+          width="30px"
+          height="30px"
+          className="searchBtn"
+          alt="searchButton"
+          onClick={handleSearchOnClick}
+        />
       </div>
 
       <div className="routeInitComponent">
